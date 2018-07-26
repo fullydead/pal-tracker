94f5c63ba8e15f81d23908a9013a92505a24e247
richard <fullydead@hotmail.com>
2018-07-26 14:59:22 +0800

    pipeline

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController {
 
+    WelcomeController(@Value("${WELCOME_MESSAGE}")){
+    }
+
     @GetMapping("/")
     public String sayHello() {
         return "hello";
